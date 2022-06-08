b5403f2bab3f02c4dd7f05c33e0427a42ad674f5
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-09 01:25:19 +0530

    Stage 1 complete

--- src/get_data.py
+++ src/get_data.py
@@ -19,7 +19,7 @@ def get_data(config_path):
     df = pd.read_csv(data_path)
     return df
 
-
+## code starting point
 if __name__ == '__main__':
     arg = argparse.ArgumentParser()
     arg.add_argument("--config", default="parameters.yaml")
---
+++ src/load_data.py
@@ -0,0 +1,21 @@
+## read the data from the data source
+## save it in data\\raw for further processing
+
+import os
+from get_data import read_params, get_data
+import argparse
+
+def load_and_save(config_path):
+    config = read_params(config_path)
+    df = get_data(config_path)
+    new_columns = [column.replace(" ","_") for column in df.columns]
+    raw_data_path = config["load_data"]["raw_dataset_csv"]
+
+    df.to_csv(raw_data_path,sep=",",index=False,header=new_columns)
+    
+
+if __name__ == '__main__':
+    arg = argparse.ArgumentParser()
+    arg.add_argument("--config", default="parameters.yaml")
+    parsed_args = arg.parse_args()
+    load_and_save(config_path=parsed_args.config)
