e442a38549910aee79603c929e9d7a5606a36e18
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-10 02:06:12 +0530

    Added a method which checks for the range of input values

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,6 +1,15 @@
-
+import pytest
 ## always start the function name with 'test' so that pytest can identify it
-def test_generic():
-    a = 2
-    b = 2 
-    assert a != b
+
+
+class NotInRangeError(Exception):
+    
+    def __init__(self,message="Value not in range"):
+        self.message = message
+        super().__init__(self.message)
+
+def test_range_of_input_values():
+    a = 5 
+    with pytest.raises(NotInRangeError):
+        if a not in range(10,20):
+            raise NotInRangeError
