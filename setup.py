e5f5b11a81891dc1acd3816d08d80ee778f309c4
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-10 00:14:34 +0530

    Created a setup python file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="src",
+    version="0.0.1",
+    description="This is a wine quality prediction packeage",
+    author="Shivam Shinde",
+    packages=find_packages(),
+    license="MIT License"
+)
