3edc830c3ec9e3806fdf385d5a637178e273f96f
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-11 00:36:01 +0530

    Added a test which check whether the feature value lies in the correct range

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,15 +1,16 @@
 import pytest
-## always start the function name with 'test' so that pytest can identify it
+# always start the function name with 'test' so that pytest can identify it
 
 
 class NotInRangeError(Exception):
-    
-    def __init__(self,message="Value not in range"):
+
+    def __init__(self, message="Value not in range"):
         self.message = message
         super().__init__(self.message)
 
+
 def test_range_of_input_values():
-    a = 5 
+    a = 5
     with pytest.raises(NotInRangeError):
-        if a not in range(10,20):
+        if a not in range(10, 20):
             raise NotInRangeError
