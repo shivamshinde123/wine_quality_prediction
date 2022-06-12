033d7321d663b17941c0a97869831c9114445f4f
Shivam Shinde <shivamshinde92722@gmail.com>
2022-06-12 14:44:34 +0530

    Updated app.py file

--- app.py
+++ app.py
@@ -3,9 +3,8 @@ import os
 import yaml
 import joblib
 import numpy as np
-from src.get_data import read_params
+from  prediction_service.prediction  import Predict, api_response, form_response
 
-params_path = "params.yaml"
 webapp_root = "webapp"
 
 static_dir = os.path.join(webapp_root, "static")
@@ -13,42 +12,24 @@ template_dir = os.path.join(webapp_root, "templates")
 
 app = Flask(__name__, static_folder=static_dir, template_folder=template_dir)
 
-def predict(data):
-    config = read_params(params_path)
-    model_dir_path = config['webapp_model_dir']
-    model = joblib.load(model_dir_path)
-    predictions = model.predict(data)
-    return predictions[0]
 
-def api_response():
-    try:
-        data = np.array([list(request.json.values())])
-        response = predict(data)
-        response = f"response: {response}"
-        return response
-    except Exception as e:
-        print(e)
-        error = "Error: Something went wrong!! Try again!"
-        return error
 
 @app.route("/", methods=["GET", "POST"])
 def index():
     if request.method == "POST":
         try:
             if request.form:
-                data = dict(request.form).values()
-                data = [list(map(float, data))]
-                response = predict(data)
+                data_req = dict(request.form)
+                response = form_response(data_req)
                 return render_template("index.html", response=response)
 
             elif request.json:
-                response = api_response(request)
+                response = api_response(request.json)
                 return jsonify(response)
 
         except Exception as e:
-            # print(f"Error occured: {str(e)}")
-            # return render_template("404.html")
-            pass
+            print(e)
+            return render_template("404.html", error=e)
     else:
         return render_template("index.html")
 
